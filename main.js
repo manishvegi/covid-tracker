fed4fe5a7e0f2899b498f8f3d9b3af77fac1c9f3
Manish <manishvegi77@gmail.com>
2020-04-19 05:17:33 +0530

    second commit

--- main.js
+++ main.js
@@ -1,45 +1,117 @@
 var btn = document.getElementById("btn1");
-
-// console.log(btn)
-btn.addEventListener('click' , () =>{
+var con = 0;
+var country;
+(function () {  
 
     fetch('https://corona.lmao.ninja/v2/countries')
     .then((response) =>{
         return (response.json());
     })
     .then((data) => {
-        var result = document.getElementById('result')
+
         // console.log(data)
-        var template = `<h3 class='card text-center'> COVID-19 Cases </h3>
-              
+        var template = ` 
+        <div class= 'row justif-content-center ml-3'>
         `
+        var result = document.getElementById('result')
+var submen = document.getElementById('submen')
+
         data.forEach((element) => {
+            var a = document.createElement('a')
+            var countryId = `${element.country}`
+            countryId = countryId.replace(/\s/g, '')
+            console.log(countryId)
+            a.appendChild(document.createTextNode(countryId))
+            a.setAttribute('class','dropdown-item')
+            a.setAttribute('id',`${element.country}`)
+            submen.appendChild(a)
+            submen.style.height = "290px"
+            submen.style.overflow = "scroll"
+          
+        country = countryId + '1';
+        
+        template += `
+        <div class= "col-lg-12  crazy text-center "  id="` + country + `">
+        <div class="list-group ">
+        <a href="#" class="list-group-item list-group-item-action active">
+        <strong>Country: ${element.country}</strong>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action "><strong>Total Cases: <span style="color:orange;font-size:20px;">${element.cases}</span></strong></a>
+        <a href="#" class="list-group-item list-group-item-action"><strong>Total Deaths: <span style="color:red;font-size:20px;">${element.deaths}</span></strong></a>
+        <a href="#" class="list-group-item list-group-item-action "><strong>Active Cases: <span style="color:orange;font-size:20px;">${element.active}</span></strong></a>
+        <a href="#" class="list-group-item list-group-item-action"><strong>Recovered: <span style="color:green;font-size:20px;">${element.recovered}</span></strong></a>
+        </div>
+        </div> 
+        </div>`
             
-            template += `
-            
-            <div class= "col-lg-3 m-2">
-            <div class="list-group ">
-      <a href="#" class="list-group-item list-group-item-action active">
-      <strong>Country: ${element.country}</strong>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action"><strong>Cases: <span style="color:orange;font-size:20px;">${element.cases}</span></strong></a>
-      <a href="#" class="list-group-item list-group-item-action"><strong>Deaths: <span style="color:red;font-size:20px;">${element.deaths}</span></strong></a>
-      <a href="#" class="list-group-item list-group-item-action"><strong>Recovered: <span style="color:green;font-size:20px;">${element.recovered}</span></strong></a>
-   
-      </div>
-    </div>
-    
-    
-            `
         });
+
+        template += `</div>`
+        submen.addEventListener('click',() =>{
+            window.onclick = e => {
+
+                var val = e.target.innerText + '1'   
+                val = val.replace(/ .*/,"")
+                if((val.slice(-1)) == '1' ){
+                con = document.getElementById(val)
+                sleep(500).then(() => {
+                con.classList.add('animated', 'bounceInLeft')
+                con.style.display = "block"
+            })
+                
+                 
+                }
+                else {
+                    sleep(500).then(() => {
+                        con.classList.add('animated', 'bounceInLeft')
+                        con.style.display = "block"
+                    })
+                }
+                
+
+
+            }
+            function sleep (time) {
+                return new Promise((resolve) => setTimeout(resolve, time));
+              }
+              
+              // Usage!
+              
+            var prev = con
+            if(con == 0 ){
+
+            }
+            else{
+            prev.classList.add('animated','bounceOutRight')
+            sleep(500).then(() => {
+                // Do something after the sleep!
+                prev.style.display = 'none'
+            });
+            
+            }
+            
+        })
         result.innerHTML =template
     })
 
 
-})
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 mybutton = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -56,3 +128,15 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+{/* <div class= "col-lg-3 m-2">
+<div class="list-group ">
+<a href="#" class="list-group-item list-group-item-action active">
+<strong>Country: ${element.country}</strong>
+</a>
+<a href="#" class="list-group-item list-group-item-action"><strong>Cases: <span style="color:orange;font-size:20px;">${element.cases}</span></strong></a>
+<a href="#" class="list-group-item list-group-item-action"><strong>Deaths: <span style="color:red;font-size:20px;">${element.deaths}</span></strong></a>
+<a href="#" class="list-group-item list-group-item-action"><strong>Recovered: <span style="color:green;font-size:20px;">${element.recovered}</span></strong></a>
+
+</div>
+</div> */}
